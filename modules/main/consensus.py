9ae6c92beac306456e5337d75e4dd7ad85503507
navidmadannezhad <navidproject283@gmail.com>
2021-11-06 06:48:42 +0330

    basic mining with 1 zero seems working in consensus module

--- modules/main/consensus.py
+++ modules/main/consensus.py
@@ -1,28 +1,9 @@
 import uuid
 from cryptography.hazmat.primitives import hashes
 
-def block_hash(prevBlock_hash, block_data):
-    hasher = hashes.Hash(hashes.SHA256())
-    thisIsGenesisBlock = prevBlock_hash is None
-
-    if thisIsGenesisBlock:
-        hasher.update(bytes(str(data), 'utf-8'))
-        hasher.update(bytes(str(nonce), 'utf-8'))
-    else:
-        hasher.update(bytes(str(data), 'utf-8'))
-        hasher.update(bytes(str(prevBlockHash), 'utf-8'))
-        hasher.update(bytes(str(nonce), 'utf-8'))
-
-    return hasher.finalize()
-
-
-def nonce(prevBlock_hash, block_data):
-    
-    desired_nonce = '111'
-    return desired_nonce
-
-class Block_hash():
+class Hash_maker():
     block_data = None
+    block_hash = None
     prevBlock_hash = None
     nonce = None
 
@@ -31,31 +12,40 @@ class Block_hash():
         self.prevBlock_hash = prevBlock_hash
 
     def generate_hash(self):
-        hasher = hashes.Hash(hashes.SHA256())
         thisIsGenesisBlock = self.prevBlock_hash is None
 
-        if thisIsGenesisBlock:
-            hasher.update(bytes(str(self.block_data), 'utf-8'))
-            # hasher.update(bytes(str(nonce), 'utf-8'))
-        else:
-            hasher.update(bytes(str(self.block_data), 'utf-8'))
-            hasher.update(bytes(str(self.prevBlock_hash), 'utf-8'))
-            # hasher.update(bytes(str(nonce), 'utf-8'))
-        # turns /x into normal hex (when data is in byte form)
-        return hasher.finalize().hex()
+        while True:
+            hasher = hashes.Hash(hashes.SHA256())
+            if thisIsGenesisBlock:
+                hasher.update(bytes(str(self.block_data), 'utf-8'))
+                hasher.update(bytes(str(self.generate_nonce()), 'utf-8'))
+            else:
+                hasher.update(bytes(str(self.block_data), 'utf-8'))
+                hasher.update(bytes(str(self.prevBlock_hash), 'utf-8'))
+                hasher.update(bytes(str(self.generate_nonce()), 'utf-8'))
+            # turns /x into normal hex (when data is in byte form)
+            self.block_hash = hasher.finalize().hex()
+            print(self.block_hash)
+            if self.hash_is_good():
+                return self.block_hash
+
 
     def generate_nonce(self):
         return uuid.uuid4().hex
 
-    def hash_is_good():
-        pass
+    def hash_is_good(self):
+        must_zero_part = self.block_hash[0:1]
+        for i in must_zero_part:
+            if i is not 0:
+                return False
+        return True
     
 
 
 # Driver Code
 if __name__ == "__main__":
 
-    hasher = Block_hash(prevBlock_hash="11", block_data="hi")
+    hasher = Hash_maker(prevBlock_hash="11", block_data="hi")
     myHash = hasher.generate_hash()
     print(myHash)
         
