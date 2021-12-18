97e40e4aaeca4513b407d111b67cc81f783d3c42
navidmadannezhad <navidproject283@gmail.com>
2021-12-18 06:20:13 +0330

    something added

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ dlt_file = 'ledger'
 dlt_format = 'dat'
 dlt_path = os.path.join(dlt_directory, dlt_file)
 
-<<<<<<< HEAD
 # client keys file information -----------------------------------
 keys_directory = './wallet/'
 private_key_file = 'private-key'
@@ -22,8 +21,3 @@ consensus_leading_zeros = 1
 # connection data -----------------------------------
 miner_port = 445
 wallet_port = 135
-=======
-consensus_leading_zeros = 3
-
-max_tx_in_block = 2
->>>>>>> 2f41aa7139664aadd3a18a347494ab16beacc7bb
--- miner/miner.py
+++ miner/miner.py
@@ -2,19 +2,39 @@ from modules.main.socket_utils import Connection
 from config import max_tx_in_block
 from modules.blockchain import Blockchain
 
-my_connection = Connection('localhost', 5500, 1024)
-my_connection.make_ready()
 
-for i in range(2):
-    tx_list = []
-    blockchain1 = Blockchain()
-    data = my_connection.recieve_data()
-    # print(max_tx_in_block)
-    for tx in data:
-        tx_list.append(tx)
-        if len(tx_list) is max_tx_in_block:
-            blockchain1.createBlock(tx_list)
-            print(blockchain1.blockList)
-            # my_connection.terminate()
 
-    # my_connection.terminate()
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
+# 1 - recieve txs from wallet
+
+
+# 2 - verify txs
+
+
+# 3 - find the longest blockchain
+
+
+# 4 - verify the blockchain
+
+
+# 5 - put txs in block, find nonce
+
+
+# 6 - set tx in ledger
+
+
+# 7 - send the ledger to all wallets
+
+
